48ecaeb509f19f40eb9d55fc839ae707934f8c57
waleed1857 <waleed.cool1857@gmail.com>
2025-07-11 10:18:05 +0500

    Solve leetcode 680

---
+++ 2 Pointers/26. Remove Duplicates from Sorted Array/code.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        return 0
+    
+obj = Solution()
+print(obj.removeDuplicates([1,1,2]))                # 2
+print(obj.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))  # 5
--- 2 Pointers/680. Valid Palindrome II/code.py
+++ 2 Pointers/680. Valid Palindrome II/code.py
@@ -1,14 +1,53 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
 
+        n = len(s)
+
         # Edge case
-        if len(s) == 1:
+        if n == 1:
             return True
         
-        return True
+
+        def isPalindrome(s, left, right):
+
+            while left <= right:
+            
+                # Return False as per palindrome condition
+                if s[left] != s[right]:
+                    return False
+                
+                # Moving the pointers
+                left += 1
+                right -= 1
+        
+            return True     # After looping, return True for a palindrome
+
+
+
+        # 2 Pointers pointing at start and end of string
+        left = 0
+        right = n - 1
+
+        # Condition forr looping over the string of even & odd length
+        while left <= right:
+            
+            # As per condition of removing at most 1 char, we declare helping function for checking
+            if s[left] != s[right]:
+
+                # We have 2 conditions, either the different char is on left side or right side
+                return isPalindrome(s, left+1, right) or isPalindrome(s, left, right-1)
+        
+            # Moving the pointers
+            left += 1
+            right -= 1
+
+        return True  # After looping, return True for a palindrome
 
 
 obj = Solution()
-print(obj.validPalindrome("aba"))       # True
 print(obj.validPalindrome("abca"))      # True
+print(obj.validPalindrome("aba"))       # True
 print(obj.validPalindrome("abc"))       # False
+
+# T.C: O(N)
+# S.C: O(1)
