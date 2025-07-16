9038d1c8f2b2b3b6aa6758f8b578681e410ec9d0
waleed1857 <waleed.cool1857@gmail.com>
2025-07-16 13:27:15 +0500

    Add leetcode-42

--- 2 Pointers/42. Trapping Rain Water/code.py
+++ 2 Pointers/42. Trapping Rain Water/code.py
@@ -2,8 +2,99 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        pass
+        
+        n = len(height)
+
+        trapped_water = 0       # Store total water
+
+        # 2 Pointers
+        left = 0
+        right = n - 1
+
+        # Variables to track maximum height to left & right side
+        max_left, max_right = height[left], height[right]
+
+
+        while left < right:
+
+            # For calculating the water on left side
+            if max_left < max_right:
+
+                left += 1       # Updating bcz we have already check the 1st height, outside the loop
+
+                # If current height is greater, update it. Otherwise Calculate the capacity
+                if height[left] >= max_left:
+                    max_left = height[left]
+                else:
+                    trapped_water += max_left - height[left]
+
+            
+            # For calculating the water on right side
+            else:
+                
+                right -= 1       # Updating bcz we have already check the 1st height, outside the loop
+
+                # If current height is greater, update it. Otherwise Calculate the capacity
+                if height[right] >= max_right:
+                    max_right = height[right]
+                else:
+                    trapped_water += max_right - height[right]
+                
+
+        return trapped_water
+
 
 obj = Solution()
 print(obj.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))   # 6     
 print(obj.trap([4, 2, 0, 3, 2, 5]))                     # 9
+
+# T.C: O(N)
+# S.C: O(1)
+
+
+
+
+
+
+'''
+from typing import List
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        n = len(height)
+
+        width = 1
+
+        trapped_water = 0
+
+        for i in range(n-1):
+
+            prev = i - 1
+            next = i + 1
+
+            if prev < 0 or next is None:
+                continue 
+
+            # For empty bar space
+            elif height[i] == 0:
+
+                if width == prev and width < next:
+                    trapped_water += 1
+
+            # previous & next values are equal
+            elif height[prev] == height[next]:
+
+                if height[i] < height[prev]:
+                    trapped_water += 1
+        
+
+        return trapped_water
+
+
+
+
+obj = Solution()
+print(obj.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))   # 6     
+print(obj.trap([4, 2, 0, 3, 2, 5]))                     # 9
+'''
---
+++ strings/344. Reverse String/code.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    # def reverseString(self, s: List[str]) -> None:
+    def reverseString(self, s: List[str]):
+        """
+        Do not return anything, modify s in-place instead.
+        """
+
+obj = Solution()
+print(obj.reverseString(["o", "l", "l", "e", "h"]))
+print(obj.reverseString(["H", "a", "n", "n", "a", "h"]))        # ["h", "a", "n", "n", "a", "H"]
