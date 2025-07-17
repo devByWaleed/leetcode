ef02cacbded8ab81ee8870536fdde72bad3bc709
waleed1857 <waleed.cool1857@gmail.com>
2025-07-17 11:39:18 +0500

    Add leetcode-344

---
+++ strings/1768. Merge Strings Alternately/code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        pass
+        
+
+obj = Solution()
+print(obj.mergeAlternately("abc", "pqr"))       # apbqcr
+print(obj.mergeAlternately("ab", "pqrs"))       # apbqrs
+print(obj.mergeAlternately("abcd", "pq"))       # apbqcd
--- strings/344. Reverse String/code.py
+++ strings/344. Reverse String/code.py
@@ -6,7 +6,30 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        n = len(s)
+
+        if n == 1:  return s    # Edge case
+
+        # Pointers at start & end
+        left = 0
+        right = n - 1
+
+        # 2 Pointers condition
+        while left < right:
+
+            # Swapping
+            s[left], s[right] = s[right], s[left]
+
+            # Updating the pointers
+            left += 1
+            right -= 1
+
+        return s
+
 
 obj = Solution()
-print(obj.reverseString(["o", "l", "l", "e", "h"]))
+print(obj.reverseString(["h", "e", "l", "l", "o"]))             # ["o", "l", "l", "e", "h"]
 print(obj.reverseString(["H", "a", "n", "n", "a", "h"]))        # ["h", "a", "n", "n", "a", "H"]
+
+# T.C: O(N)
+# S.C: O(1)
