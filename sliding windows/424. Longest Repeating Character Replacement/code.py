d709ed557e5c3f370c6c4035b675b06c694ea54f
waleed1857 <waleed.cool1857@gmail.com>
2025-07-23 12:03:17 +0500

    Add leetcode-796

---
+++ sliding windows/424. Longest Repeating Character Replacement/code.py
@@ -0,0 +1,7 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        pass
+
+obj = Solution()
+print(obj.characterReplacement("ABAB", 2))      # 4
+print(obj.characterReplacement("AABABBA", 1))   # 4
--- strings/796. Rotate String/code.py
+++ strings/796. Rotate String/code.py
@@ -1,8 +1,44 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        pass
+        
+        for _ in range(len(s)):
+
+            # Rotating 1st char to the end
+            s = s[1:] + s[0]
+
+            # Checking for desired result
+            if s == goal:   return True
+
+        # If not found, return False
+        return False
+
 
 
 obj = Solution()
 print(obj.rotateString("abcde", "cdeab"))       # True
 print(obj.rotateString("abcde", "abced"))       # False
+
+# T.C: O(N ^ 2)
+# S.C: O(1)
+
+
+
+# ******** or
+'''
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        
+        # Concatenation of s
+        s = s + s
+
+        # Checking for existense
+        return goal in s
+
+
+obj = Solution()
+print(obj.rotateString("abcde", "cdeab"))       # True
+print(obj.rotateString("abcde", "abced"))       # False
+
+# T.C: O(N)
+# S.C: O(1)
+'''
