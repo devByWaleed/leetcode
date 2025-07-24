d70a1a8d39acd0f996cfa16d646322f692783f26
waleed1857 <waleed.cool1857@gmail.com>
2025-07-24 14:06:34 +0500

    Try leetcode-424

--- sliding windows/424. Longest Repeating Character Replacement/code.py
+++ sliding windows/424. Longest Repeating Character Replacement/code.py
@@ -1,7 +1,38 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        pass
+        
+        max_subString = 0   # save longest sub-string length
+        current_len = 1    # for starting with 1 index, current should be 1
+
+        s = list(s)     # for replacement
+
+        for i in range(1, len(s)):
+            
+            # If current and previous is same
+            if s[i] == s[i-1]:  
+                current_len += 1
+                if max_subString < current_len:     max_subString = current_len
+                continue
+
+            # Codition for replacement
+            elif s[i] != s[i-1] and k != 0:
+                s[i] = s[i-1]
+                current_len += 1
+                k -= 1
+                max_subString = max(max_subString, current_len)
+
+            # If not same but we have no chance to replace
+            elif s[i] != s[i-1] and k == 0:
+                max_subString = max(max_subString, current_len)
+                break       # when k = 0, will no replace so loop should be break
+
+        return max_subString
+
 
 obj = Solution()
+print(obj.characterReplacement("AABA", 0))      # 1 but 2
 print(obj.characterReplacement("ABAB", 2))      # 4
 print(obj.characterReplacement("AABABBA", 1))   # 4
+print(obj.characterReplacement("AAAA", 2))      # 4
+
+
