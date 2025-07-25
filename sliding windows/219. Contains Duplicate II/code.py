46a1f590738412dff6e7ccfc1343cec59ca08693
waleed1857 <waleed.cool1857@gmail.com>
2025-07-25 11:28:33 +0500

    Add leetcode-424

---
+++ sliding windows/219. Contains Duplicate II/code.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        pass
+
+obj = Solution()
+print(obj.containsNearbyDuplicate([1, 2, 3, 1], 3))           # True
+print(obj.containsNearbyDuplicate([1, 0, 1, 1], 1))           # True
+print(obj.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2))     # False
--- sliding windows/424. Longest Repeating Character Replacement/code.py
+++ sliding windows/424. Longest Repeating Character Replacement/code.py
@@ -1,6 +1,46 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         
+        max_length = 0   # save longest sub-string length
+        max_count = 0   # max frequency for single char in current window
+        left = 0
+
+        frequencies = {}    # Track frequecies for each window 
+
+        for right in range(len(s)):
+
+            # Update the frequency
+            frequencies[s[right]] = frequencies.get(s[right], 0) + 1
+
+            # Update current frequency of window
+            if max_count < frequencies[s[right]]:    max_count = frequencies[s[right]]
+
+            # Contracting window size and update pointer
+            while (right - left + 1) - max_count > k:
+                frequencies[s[left]] -= 1
+                left += 1
+
+            # Update maximum length after window shrinking
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
+
+
+obj = Solution()
+print(obj.characterReplacement("ABAB", 2))      # 4
+print(obj.characterReplacement("AABABBA", 1))   # 4
+print(obj.characterReplacement("AAAA", 2))      # 4
+print(obj.characterReplacement("ABAA", 0))      # 2
+
+# T.C: O(N)
+# S.C: O(1)
+
+
+# First Version (not completed)
+'''
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
         max_subString = 0   # save longest sub-string length
         current_len = 1    # for starting with 1 index, current should be 1
 
@@ -24,15 +64,14 @@ class Solution:
             # If not same but we have no chance to replace
             elif s[i] != s[i-1] and k == 0:
                 max_subString = max(max_subString, current_len)
-                break       # when k = 0, will no replace so loop should be break
+                # break       # when k = 0, will no replace so loop should be break
 
         return max_subString
 
 
 obj = Solution()
-print(obj.characterReplacement("AABA", 0))      # 1 but 2
-print(obj.characterReplacement("ABAB", 2))      # 4
 print(obj.characterReplacement("AABABBA", 1))   # 4
+print(obj.characterReplacement("ABAA", 0))      # 1 but 2
+print(obj.characterReplacement("ABAB", 2))      # 4
 print(obj.characterReplacement("AAAA", 2))      # 4
-
-
+'''
