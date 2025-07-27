9b541d85d76041a0b27ebb26d7c9907fbb1faaa3
waleed1857 <waleed.cool1857@gmail.com>
2025-07-27 16:32:42 +0500

    Add leetcode-3191

---
+++ sliding windows/3. Longest Substring Without Repeating Characters/code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        pass
+
+obj = Solution()
+print(obj.lengthOfLongestSubstring("abcabcbb"))     # 3
+print(obj.lengthOfLongestSubstring("bbbbb"))        # 1
+print(obj.lengthOfLongestSubstring("pwwkew"))       # 3
--- sliding windows/3191. Minimum Operations to Make Binary Array Elements Equal to One I/code.py
+++ sliding windows/3191. Minimum Operations to Make Binary Array Elements Equal to One I/code.py
@@ -2,7 +2,30 @@ from typing import List
 
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        pass
+        
+        n = len(nums)
+        operations = 0  # To count no. of operations
+        
+        # Loop range for not getting out of bound error
+        for i in range(0, n - 2):
+
+            if nums[i] == 0:
+
+
+                # Selecting 3 consecutives elements and flip it
+                nums[i] = 1 - nums[i]
+                nums[i + 1] = 1 - nums[i + 1]
+                nums[i + 2] = 1 - nums[i + 2]
+                
+                # increment after flipping numbers or before not matters
+                operations += 1
+
+        # Checking for last two elements as loop not check them
+        if (nums[n - 1] == 0) or (nums[n - 2] == 0):
+            return -1   # Returning -1 as per condition (not possible to change it to 1)
+        
+        return operations
+
 
 obj = Solution()
 print(obj.minOperations([0, 1, 1, 1, 0, 0]))    # 3
