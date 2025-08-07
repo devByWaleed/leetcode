1ab5f1cce01d2cf81cc3af55205f3bc2edd7c49e
waleed1857 <waleed.cool1857@gmail.com>
2025-08-07 17:28:35 +0500

    Add leetcode-2418

---
+++ Array With Hashing/1370. Increasing Decreasing String/code.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortString(self, s: str) -> str:
+        pass
+
+obj = Solution()
+print(obj.sortString("aaaabbbbcccc"))   # abccbaabccba
+print(obj.sortString("rat"))            # art
--- Array With Hashing/2418. Sort the People/code.py
+++ Array With Hashing/2418. Sort the People/code.py
@@ -2,8 +2,36 @@ from typing import List
 
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        pass
+        
+        n = len(names)
+
+        hash_map = {}
+
+        # Loop to update the hashmap
+        for i in range(n):
+
+            hash_map.update({heights[i]: names[i]})
+        
+        # Sorting the hash_map  in descending order by key
+        hash_map = dict(sorted(hash_map.items(), key=lambda item: item[0], reverse=True))
+
+
+        # enumerate() to get index
+        for index, key in enumerate(hash_map):
+            
+            # Sort the names array according to hash_map
+            names[index] = hash_map[key]
+            
+            # Line for checking the height (debugging)
+            # heights[index] = key
+            
+
+        return names
+
 
 obj = Solution()
 print(obj.sortPeople(["Mary", "John", "Emma"], [180, 165, 170]))    # ["Mary", "Emma", "John"]
 print(obj.sortPeople(["Alice", "Bob", "Bob"], [155, 185, 150]))     # ["Bob", "Alice", "Bob"]
+
+# T.C: O(N Log N)
+# S.C: O(N)
