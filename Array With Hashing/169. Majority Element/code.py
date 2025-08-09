2259251795dc337ab1b4fbc7d29006bfb0f0adcf
waleed1857 <waleed.cool1857@gmail.com>
2025-08-09 13:05:35 +0500

    Add leetcode-1832

---
+++ Array With Hashing/169. Majority Element/code.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+print(obj.majorityElement([3, 2, 3]))                 # 3
+print(obj.majorityElement([2, 2, 1, 1, 1, 2, 2]))     # 2
--- Array With Hashing/1832. Check if the Sentence Is Pangram/code.py
+++ Array With Hashing/1832. Check if the Sentence Is Pangram/code.py
@@ -1,9 +1,25 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         
-        if len(sentence) == 1:      return True
+        hash_set = set()
+
+        for i in sentence:
+
+            # If not in hash_set, add
+            if i not in hash_set:
+                hash_set.add(i)
+            
+            # If in hash_set, ignore
+            else:
+                continue
+        
+        # Check for all alphabets length
+        return len(hash_set) == 26
 
 
 obj = Solution()
 print(obj.checkIfPangram("thequickbrownfoxjumpsoverthelazydog"))    # True
 print(obj.checkIfPangram("leetcode"))                               # False
+
+# T.C: O(N)
+# S.C: O(1)
