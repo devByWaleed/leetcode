f4571dadf659aad1717692ffa85806aff61fc2a7
waleed1857 <waleed.cool1857@gmail.com>
2025-08-13 16:22:11 +0500

    add leetcode-1160

--- Array With Hashing/1160. Find Words That Can Be Formed by Characters/code.py
+++ Array With Hashing/1160. Find Words That Can Be Formed by Characters/code.py
@@ -1,12 +1,15 @@
 from typing import List
+from collections import Counter
 
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
         
-        '''char_freq = {}
+        char_freq = {}
 
-        word_freq = {}
+        # count the length of word formed by chars
+        counter = 0
 
+        # Counting frequencies of each letter in chars
         for i in range(len(chars)):
 
             if chars[i] not in char_freq:
@@ -14,17 +17,30 @@ class Solution:
 
             char_freq[chars[i]] += 1
         
-        
-        for j in range(len(chars)):
+        # Nested loop to check for each letter
+        for j in words:
 
-            if chars[j] not in word_freq:
-                word_freq.update({chars[j]: 0})
+            # Counting frequencies of each word
+            word_freq = Counter(j)
 
-            word_freq[chars[j]] += 1
-        
-        return char_freq'''
+            can_form = True
+            for k in word_freq:
+
+                # Checking for presence based on frequencies
+                if not k in char_freq or not char_freq[k] >= word_freq[k]:
+                    can_form = False    # To skip whole word if letter not in chars
+                    break
+            
+            # If word can be formed, add its length
+            if can_form:
+                counter += len(j)
+            
+        return counter
 
 
 obj = Solution()
 print(obj.countCharacters(["cat", "bt", "hat", "tree"], "atach"))               # 6
 print(obj.countCharacters(["hello", "world", "leetcode"], "welldonehoneyr"))    # 10
+
+# T.C: O(N + M)
+# S.C: O(N + M)
---
+++ Array With Hashing/229. Majority Element II/code.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+print(obj.majorityElement([3, 2, 3]))       # [3]
+print(obj.majorityElement([1]))             # [1]
+print(obj.majorityElement([1, 2]))          # [1, 2]
