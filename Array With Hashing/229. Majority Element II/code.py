3c19c8773f9b68ebcc338961baf2599c398a7bd0
waleed1857 <waleed.cool1857@gmail.com>
2025-08-14 19:58:54 +0500

    Add leetcode-229

--- Array With Hashing/229. Majority Element II/code.py
+++ Array With Hashing/229. Majority Element II/code.py
@@ -2,9 +2,38 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        pass
-
+        
+        n = len(nums)
+         
+        # Frequency target for checking to each element 
+        target = n // 3
+         
+        frequencies = {}
+         
+        array = []    # To store majority elements 
+         
+        # Storing frequencies of each element 
+        for i in nums:
+             
+            if i not in frequencies:
+                frequencies.update({i: 0})
+             
+            frequencies[i] += 1
+             
+         
+        for j in frequencies:
+             
+            # If we find majority element, add it to array
+            if frequencies[j] > target:
+                array.append(j)
+        
+        return array
+        
+        
 obj = Solution()
 print(obj.majorityElement([3, 2, 3]))       # [3]
 print(obj.majorityElement([1]))             # [1]
 print(obj.majorityElement([1, 2]))          # [1, 2]
+
+# T.C: O(N)
+# S.C: O(N)
---
+++ sliding windows/643. Maximum Average Subarray I/code.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        pass
+
+obj = Solution()
+print(obj.findMaxAverage([1, 12, -5, -6, 50, 3], 4))    # 12.75000
+print(obj.findMaxAverage([5], 1))                       # 5.00000
