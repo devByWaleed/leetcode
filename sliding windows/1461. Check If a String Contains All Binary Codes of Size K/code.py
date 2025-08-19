a41f49aeac77f7d1cede30ce4558933dc4a3292e
waleed1857 <waleed.cool1857@gmail.com>
2025-08-19 12:09:52 +0500

    Add leetcode-219

---
+++ sliding windows/1461. Check If a String Contains All Binary Codes of Size K/code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        pass
+
+obj = Solution()
+print(obj.hasAllCodes("00110110", 2))       # True
+print(obj.hasAllCodes("0110", 1))           # True
+print(obj.hasAllCodes("0110", 2))           # False
--- sliding windows/219. Contains Duplicate II/code.py
+++ sliding windows/219. Contains Duplicate II/code.py
@@ -2,9 +2,57 @@ from typing import List
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        pass
+        
+        n = len(nums)
+        
+        # Empty hashset of size k
+        window = set()
+
+        for i in range(n):
+
+            # Checking for duplicates (1st condition)
+            if nums[i] in window:
+                return True
+            
+            # add if not present
+            window.add(nums[i])
+
+            # Keeping the window of size k by removing old element, enforce 2nd condition
+            if i >= k:
+                window.discard(nums[i-k])
+
+        # If after iterating all over array, return False
+        return False
+
 
 obj = Solution()
 print(obj.containsNearbyDuplicate([1, 2, 3, 1], 3))           # True
 print(obj.containsNearbyDuplicate([1, 0, 1, 1], 1))           # True
 print(obj.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2))     # False
+
+# T.C: O(N)
+# S.C: O(K)
+
+
+
+# Nested loop (brute force thinking)
+'''from typing import List
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        
+        for i in range(len(nums)):
+
+            for j in range(len(nums)):
+
+                if i == j:  continue
+
+                if nums[i] == nums[j] and abs(i - j) <= k:
+                    return True
+        
+        return False
+
+obj = Solution()
+print(obj.containsNearbyDuplicate([1, 2, 3, 1], 3))           # True
+print(obj.containsNearbyDuplicate([1, 0, 1, 1], 1))           # True
+print(obj.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2))     # False'''
