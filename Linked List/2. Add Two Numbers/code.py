44f1eda35fa46a9c7c93de3159fb9abbb45363bd
waleed1857 <waleed.cool1857@gmail.com>
2025-10-01 15:50:31 +0500

    Add leetcode-2

--- Linked List/2. Add Two Numbers/code.py
+++ Linked List/2. Add Two Numbers/code.py
@@ -13,7 +13,45 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        
+        dummy = ListNode(0)
+
+        curr = dummy
+
+        carry = 0       # Carry if sum is >= 10
+
+        while l1 or l2 or carry:
+            
+            # Step 1.1: Read values from l1 and l2 (0 if list exhausted)
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            # Step 1.2: Sum the two digits + carry
+            total = val1 + val2 + carry
+
+            # Step 1.3: Determine new digit and new carry
+            carry = total // 10
+            new_digit = total % 10
+            
+            # Adding new node with sum value
+            curr.next = ListNode(new_digit)
+
+            # Traversing the lists 
+            curr = curr.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        # For printing whole Linked-List
+        '''
+        while dummy.next:
+            print(dummy.next, end=" -> ")
+            dummy = dummy.next
+        '''
+
+        return dummy.next
 
 
 obj = Solution()
@@ -29,10 +67,8 @@ print(obj.addTwoNumbers(head1, head2))      # 7 -> 0 -> 8
 
 
 head1 = ListNode(0)
-head1.next = ListNode(None)
 
 head2 = ListNode(0)
-head2.next = ListNode(None)
 print(obj.addTwoNumbers(head1, head2))      # 0
 
 
@@ -49,3 +85,6 @@ head2.next = ListNode(9)
 head2.next.next = ListNode(9)
 head2.next.next.next = ListNode(9)
 print(obj.addTwoNumbers(head1, head2))      # 8 -> 9 -> 9 -> 9 -> 0 -> 0 -> 0 -> 1
+
+# T.C: O(Max(M,N))
+# S.C: O(Max(M,N))
---
+++ Linked List/328. Odd Even Linked List/code.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    # Customize this to show node value during de-bugging
+    def __repr__(self): 
+        return f"{self.val}"
+
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+
+obj = Solution()
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+print(obj.oddEvenList(head))            # 1 -> 3 -> 5 -> 2 -> 4
+
+
+head = ListNode(2)
+head.next = ListNode(1)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(5)
+head.next.next.next.next = ListNode(6)
+head.next.next.next.next.next = ListNode(4)
+head.next.next.next.next.next.next = ListNode(7)
+print(obj.oddEvenList(head))            # 2 -> 3 -> 6 -> 7 -> 1 -> 5 -> 4
