37178f59e332308fa0e4065d674ce05be90835be
waleed1857 <waleed.cool1857@gmail.com>
2025-10-06 15:16:23 +0500

    leetcode-202 (unsolved)

--- strings/202. Happy Number/code.py
+++ strings/202. Happy Number/code.py
@@ -1,52 +1,98 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         
-        square_sum = 0
+        square_sum = 0  # Calculate sum of digit's square
 
-        string = str(n)
+        string = str(n)     # int to string for iteration
 
-        checked = set()
+        checked = set()     # Keep track of seen numbers
 
-        hash_map = {}
-
-        flag = True
-        while flag:
-
+        hash_map = {}       # To map digit & its square
+ 
+        while True:
+            # Mapping value to hashmap
             for i in range(len(string)):
                 hash_map[string[i]] = int(string[i]) * int(string[i])
 
-
+            # Calculate sum
             for num in hash_map:
                 square_sum += hash_map[num]
             
+            # Condition if sum equals to 1
+            if square_sum == 1:
+                return True                
 
-            if square_sum not in checked:
-                checked.add(square_sum)
-            elif square_sum  in checked:
+            # Condition if sum equals to 1
+            elif square_sum in checked:
                 return False
-                           
-            elif square_sum != 1 and square_sum != n:
+            
+            # Else continue for the number
+            else:
+                checked.add(square_sum)
                 hash_map.clear()
 
                 string = str(square_sum)
 
                 square_sum = 0
 
-            
-            else:
-                return True
-
-
-        # return False
 
 obj = Solution()
-print(obj.isHappy(2))       # False
-print(obj.isHappy(19))      # True
+print(obj.isHappy(19))       # True
+print(obj.isHappy(2))        # False
+print(obj.isHappy(11))       # False, but True given
 
-print(obj.isHappy(7))      # True
-print(obj.isHappy(10))      # True
-print(obj.isHappy(13))      # True
 
-print(obj.isHappy(4))       # False
-print(obj.isHappy(20))       # False
-print(obj.isHappy(8))       # False
+
+
+
+
+
+
+
+# class Solution:
+#     def isHappy(self, n: int) -> bool:
+        
+#         square_sum = 0  # Calculate sum of digit's square
+
+#         string = str(n)
+
+#         checked = set()     # Keep track of seen numbers
+
+#         hash_map = {}       # To map digit & its square
+ 
+#         while True:
+            
+#             # Mapping value to hashmap
+#             for i in range(len(string)):
+#                 hash_map[string[i]] = int(string[i]) * int(string[i])
+
+#             # Calculate sum
+#             for num in hash_map:
+#                 square_sum += hash_map[num]
+            
+#             # Condition if sum equals to 1
+#             if square_sum not in checked:
+#                 checked.add(square_sum)
+#                 hash_map.clear()
+
+#                 string = str(square_sum)
+
+#                 square_sum = 0
+
+#             elif square_sum in checked:
+#                 break
+                
+#         return square_sum == 1 and square_sum in checked
+
+
+# obj = Solution()
+# print(obj.isHappy(7))      # True
+# print(obj.isHappy(19))      # True
+# print(obj.isHappy(2))       # False
+
+# print(obj.isHappy(10))      # True
+# print(obj.isHappy(13))      # True
+
+# print(obj.isHappy(4))       # False
+# print(obj.isHappy(20))       # False
+# print(obj.isHappy(8))       # False
