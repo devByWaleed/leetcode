81745d1f51ce83803cef5880393c9197ce5c35bf
waleed1857 <waleed.cool1857@gmail.com>
2025-10-08 12:07:40 +0500

    Add leetcode-844

---
+++ stack/1047. Remove All Adjacent Duplicates In String/code.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        pass
+
+obj = Solution()
+print(obj.removeDuplicates("abbaca"))   # ca
+print(obj.removeDuplicates("azxxzy"))   # ay
--- stack/844. Backspace String Compare/code.py
+++ stack/844. Backspace String Compare/code.py
@@ -1,8 +1,35 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        pass
+        
+        s1, t1 = "", ""
+
+        # Loop on s
+        for i in range(len(s)):
+
+            if s[i] == "#" and s1:
+                # Remove last element
+                s1 = s1[:len(s1)-1]
+            elif s[i] != "#":
+                s1 += s[i]
+        
+        # Loop on t
+        for i in range(len(t)):
+            if t[i] == "#" and t1:
+                # Remove last element
+                t1 = t1[:len(t1)-1]
+            elif t[i] != "#":
+                t1 += t[i]
+
+        # Compare both strings
+        return s1 == t1
+
 
 obj = Solution()
-print(obj.backspaceCompare("ab#c", "ad#c"))     # True
-print(obj.backspaceCompare("ab##", "c#d#"))     # True
-print(obj.backspaceCompare("a#c", "b"))         # False
+print(obj.backspaceCompare("a", "aa#a"))                   # False
+print(obj.backspaceCompare("ab#c", "ad#c"))                # True
+print(obj.backspaceCompare("ab##", "c#d#"))                # True
+print(obj.backspaceCompare("a#c", "b"))                    # False
+print(obj.backspaceCompare("y#fo##f", "y#f#o##f"))         # True
+
+# T.C: O(M + N)
+# S.C: O(M + N)
