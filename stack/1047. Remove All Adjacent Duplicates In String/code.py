5a177d1fde7cb07dbaf55b0eedbb981f03089515
waleed1857 <waleed.cool1857@gmail.com>
2025-10-09 11:25:16 +0500

    Add leetcode-1047

--- stack/1047. Remove All Adjacent Duplicates In String/code.py
+++ stack/1047. Remove All Adjacent Duplicates In String/code.py
@@ -1,7 +1,58 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        pass
+        
+        n = len(s)
+
+        st = []
+
+        new_string = ""
+
+        # Manage stack
+        for i in range(n):
+            if not st or s[i] != st[-1]:
+                st.append(s[i])
+            elif s[i] == st[-1]:
+                st.pop()
+
+        # Add elements to new string
+        while st:
+            new_string += st[-1]
+            st.pop()
+
+        # Reversing new string
+        new_string = reversed(new_string)
+        return "".join(list(new_string))
+
 
 obj = Solution()
 print(obj.removeDuplicates("abbaca"))   # ca
 print(obj.removeDuplicates("azxxzy"))   # ay
+
+# T.C: O(N)
+# S.C: O(N)
+
+
+# Brute Force
+'''
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        
+        n = len(s)
+
+        new_string = ""
+
+        for i in range(n):
+            if not new_string or s[i] != new_string[-1]:
+                new_string += s[i]
+            else:
+                new_string = new_string[:-1]
+        
+        return new_string
+
+obj = Solution()
+print(obj.removeDuplicates("abbaca"))   # ca
+print(obj.removeDuplicates("azxxzy"))   # ay
+
+# T.C: O(N ^ 2)
+# S.C: O(N)
+'''
---
+++ stack/1544. Make The String Great/code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        pass
+
+
+obj = Solution()
+print(obj.makeGood("leEeetcode"))       # leetcode
+print(obj.makeGood("abBAcC"))           # ""
+print(obj.makeGood("s"))                # s
