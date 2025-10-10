454ee4564c87218ed41489e637ec80bb84930280
waleed1857 <waleed.cool1857@gmail.com>
2025-10-10 11:29:00 +0500

    Add leetcode-1544

--- stack/1544. Make The String Great/code.py
+++ stack/1544. Make The String Great/code.py
@@ -1,9 +1,39 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        pass
+        
+        # Edge case
+        if len(s) == 0 or len(s) == 1:
+            return s
+        
+        st = []
+        new_string = ""
+
+        # Handling stack with string
+        for i in range(len(s)):
+            if not st:
+                st.append(s[i])
+            else:
+                # If adjacent pair, remove from stack
+                if s[i].lower() == st[-1].lower() and s[i] != st[-1]:
+                    st.pop()
+                # Else add current to stack
+                else:
+                    st.append(s[i])
+        
+        # Creating new string
+        while st:
+            new_string += st[-1]
+            st.pop()
+
+        # Reversing the string
+        new_string = reversed(new_string)
+        return "".join(new_string)
 
 
 obj = Solution()
 print(obj.makeGood("leEeetcode"))       # leetcode
 print(obj.makeGood("abBAcC"))           # ""
 print(obj.makeGood("s"))                # s
+
+# T.C: O(N)
+# S.C: O(N)
---
+++ stack/921. Minimum Add to Make Parentheses Valid/code.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        pass
+
+obj = Solution()
+print(obj.minAddToMakeValid("())"))     # 1
+print(obj.minAddToMakeValid("((("))     # 3
