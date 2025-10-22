923d2f1025e37e011cbae459b493e1f478cb3317
waleed1857 <waleed.cool1857@gmail.com>
2025-10-22 10:40:01 +0500

    Add leetcode-1700

--- queue/1700. Number of Students Unable to Eat Lunch/code.py
+++ queue/1700. Number of Students Unable to Eat Lunch/code.py
@@ -1,28 +1,41 @@
 from typing import List
+from collections import deque
 
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         
         n = len(students)
-        take = 0
-        # infinite = 0
+        queue = deque(students)     # Using deque for efficient removal
+        stack = deque(sandwiches)
+        take = 0            # Track of student able to eat
+        infinite = 0        # To check for infinite loop
 
-        while students:
+        while queue:
 
             # If student take sandwich
-            if students[0] == sandwiches[0]:
-                students.pop(0)
-                sandwiches.pop(0)
+            if queue[0] == stack[0]:
+                queue.popleft()
+                stack.popleft()
                 take += 1
+                infinite = 0    # Re-setting for smooth record
             
             # Student will go to end of line
             else:
-                current = students.pop(0)
-                students.append(current)
+                current = queue.popleft()
+                queue.append(current)
+                infinite += 1
 
+                # Check for infinite loop
+                if infinite == len(queue):  break
+
+        # Calculating the students who are unable to eat
         unable = n - take
         return unable
 
+
 obj = Solution()
-print(obj.countStudents([1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1]))    # 3
 print(obj.countStudents([1, 1, 0, 0], [0, 1, 0, 1]))                # 0
+print(obj.countStudents([1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1]))    # 3
+
+# T.C: O(N)
+# S.C: O(N)
---
+++ queue/1823. Find the Winner of the Circular Game/code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.findTheWinner(5, 2))      # 3
+print(obj.findTheWinner(6, 5))      # 1
