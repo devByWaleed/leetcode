ac0cbca77c3aedd360cf40a173ced1db93c3bdbb
waleed1857 <waleed.cool1857@gmail.com>
2025-10-29 10:14:09 +0500

    Add leetcode-232 (brute-force)

--- Design/232. Implement Queue using Stacks/code.py
+++ Design/232. Implement Queue using Stacks/code.py
@@ -1,31 +1,82 @@
+from collections import deque
+
 class MyQueue:
 
     def __init__(self):
-        pass
+        self.st = deque()
         
 
     def push(self, x: int) -> None:
-        pass
+        self.st.append(x)
         
 
     def pop(self) -> int:
-        pass
+        top = self.st.popleft()
+        return top
         
 
     def peek(self) -> int:
-        pass
+        return self.st[0]
         
 
     def empty(self) -> bool:
-        pass
+        return len(self.st) == 0
+        
+
+# # Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(1)
+# obj.push(2)
+# print(obj.peek())       # 1
+# print(obj.pop())        # 1
+# print(obj.empty())      # False
+
+"""
+None
+None
+None
+1
+1
+False
+"""
+
+
+# Using 2 stacks
+'''
+class MyQueue:
+
+    def __init__(self):
+        self.st1 = []
+        self.st2 = []
+        
+
+    def push(self, x: int) -> None:
+        self.st1.append(x)
+        
+
+    def pop(self) -> int:
+        top = self.st1.pop()
+        self.st2.append(top)
+        return self.st1[0]
+        
+
+    def peek(self) -> int:
+        return self.st1[0]
+        
+
+    def empty(self) -> bool:
+        return len(self.st1) == 0
         
 
 # Your MyQueue object will be instantiated and called as such:
 obj = MyQueue()
 obj.push(1)
 obj.push(2)
+obj.push(3)
 print(obj.peek())       # 1
 print(obj.pop())        # 1
+print(obj.pop())        # 1
+print(obj.peek())       # 1
 print(obj.empty())      # False
 
 """
@@ -36,3 +87,4 @@ None
 1
 False
 """
+'''
