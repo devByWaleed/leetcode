4e5aa525280e2d251c9d8d1076ffc01723099a43
waleed1857 <waleed.cool1857@gmail.com>
2025-11-07 11:12:25 +0500

    Add leetcode-739

---
+++ stack/22. Generate Parentheses/code.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        pass
+
+
+obj = Solution()
+print(obj.generateParenthesis(3))       # [((())),(()()),(())(),()(()),()()()]
+print(obj.generateParenthesis(1))       # [()]
