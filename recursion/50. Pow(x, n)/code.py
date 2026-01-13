9703b196759d0c0b4dde59054d14f2b4acc817a5
waleed1857 <waleed.cool1857@gmail.com>
2026-01-13 13:54:53 +0500

    Add leetcode-50

--- recursion/50. Pow(x, n)/code.py
+++ recursion/50. Pow(x, n)/code.py
@@ -1,9 +1,40 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        pass
+        # Base cases
+        if n == 1:  return x
+        if n == 0:  return 1
+        '''
+        if n == 0 or x == 0:  return 1   # x > 0 (guaranteed)
+        '''
+
+        # If n is negative
+        if n < 0:
+            # To make n +ve
+            n = abs(n)
+
+            # Calculation
+            result = self.myPow(x, n)
+
+            # Invert the result
+            return 1 / result
+
+        else:
+            # Pre-calculate to reduce operations
+            half = self.myPow(x, n // 2)
+
+            # If n is even
+            if n % 2 == 0:
+                return half * half
+            
+            # If n is odd, remove one x
+            else:
+                return half * half * x
 
 
 obj = Solution()
 print(obj.myPow(2.00000, 10))       # 1024.00000
 print(obj.myPow(2.10000, 3))        # 9.26100
 print(obj.myPow(2.00000, -2))       # 0.25000
+
+# T.C: O(LOG(N))
+# S.C: O(LOG(N))
---
+++ recursion/R6. Check if a Number is a Palindrome/code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, n: int) -> bool:
+        pass
+
+
+obj = Solution()
+print(obj.isPalindrome(121))       # True
+print(obj.isPalindrome(123))       # False
