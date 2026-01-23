07555bf3bd292d6aa9c996d108dd9af1cef918c2
waleed1857 <waleed.cool1857@gmail.com>
2026-01-23 15:30:16 +0500

    Add leetcode-98

--- trees/98. Validate Binary Search Tree/README.md
+++ trees/98. Validate Binary Search Tree/README.md
@@ -37,7 +37,7 @@ Output `false`
 
 `The number of nodes in the tree is in the range [1, 10^4].`
 
-`-2^31 = Node.val = 2^31 - 1`
+`-2^31 <= Node.val <= 2^31 - 1`
 
 
 ### Topics
--- trees/98. Validate Binary Search Tree/code.py
+++ trees/98. Validate Binary Search Tree/code.py
@@ -14,8 +14,26 @@ class TreeNode:
 
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        pass
 
+        # Helper function to validate BST
+        def validate(node, low=-float("inf"), high=float("inf")):
+            # Edge case
+            if not node:
+                return True
+            
+            # BST condition
+            if not (low < node.val < high):
+                return False
+            
+            # Recursive call on left & right child
+            left_BST = validate(node.left, low, node.val)
+            right_BST = validate(node.right, node.val, high)
+            
+            # Return the answer
+            return (left_BST and right_BST)
+        
+        return validate(root)
+    
 
 obj = Solution()
 
@@ -35,3 +53,36 @@ root.right = TreeNode(4)
 root.right.left = TreeNode(3)
 root.right.right = TreeNode(6)
 print(obj.isValidBST(root))    # False
+
+# Test Case
+root = TreeNode(1)
+
+root.left = TreeNode(1)
+print(obj.isValidBST(root))    # True
+
+# T.C: O(N)
+# S.C: O(N)
+
+
+# Logical Issues
+'''
+# Edge case
+        if not root.left and not root.right:
+            return True
+        
+        
+        # Checking root for BST
+        rootBST = [(root.left.val < root.val and root.right.val > root.val)]
+        
+        # Base case for recursive call
+        if rootBST[0]:
+            return True
+        
+        # Checking left & right childs for BST
+        leftBST = self.isValidBST(root.left)
+        rightBST = self.isValidBST(root.right)
+        
+        # Answer based on all 3 conditions
+        return rootBST[0] and leftBST and rightBST
+
+'''
