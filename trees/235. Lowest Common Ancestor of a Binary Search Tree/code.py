84657dd8d50d003958c47bfa87dc2328cbe5cd06
waleed1857 <waleed.cool1857@gmail.com>
2026-02-03 14:37:45 +0500

    Add leetcode-235

--- trees/235. Lowest Common Ancestor of a Binary Search Tree/code.py
+++ trees/235. Lowest Common Ancestor of a Binary Search Tree/code.py
@@ -12,7 +12,38 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pass
+        # Starting with root
+        lca = [root]
+
+        def search(root):
+            # Base case
+            if not root:
+                return
+            
+            # update the value for each sub-tree
+            lca[0] = root
+
+            # If we found p, q as root, return nothing
+            if root is p or root is q:
+                return
+            
+            # If value is less, search on right side
+            elif root.val < p.val and root.val < q.val:
+                search(root.right)
+            
+            # If value is greater, search on left side
+            elif root.val > p.val and root.val > q.val:
+                search(root.left)
+            
+            # If not found, return nothing
+            else:
+                return
+            
+        # Pass to helper function
+        search(root)
+
+        # Return the node
+        return lca[0]
 
 
 obj = Solution()
@@ -51,6 +82,19 @@ root.right.right = TreeNode(9)
 root.left.right.left = TreeNode(3)
 root.left.right.right = TreeNode(5)
 
-p_node = root.left        # This is the node with value 2
-q_node = root.right       # This is the node with value 8
+p_node = root.left             # This is the node with value 2
+q_node = root.left.right       # This is the node with value 4
 print(obj.lowestCommonAncestor(root, p_node, q_node))    # 2
+
+
+# Test Case
+root = TreeNode(2)
+
+root.left = TreeNode(1)
+
+p_node = root            # This is the node with value 2
+q_node = root.left       # This is the node with value 1
+print(obj.lowestCommonAncestor(root, p_node, q_node))    # 2
+
+# T.C: O(LOG(N))
+# S.C: O(1)
