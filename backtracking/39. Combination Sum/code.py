0df8fa1f184785fdcba69125925d9dc0cd686d40
waleed1857 <waleed.cool1857@gmail.com>
2026-02-21 14:28:01 +0500

    Add leetcode-39

--- backtracking/39. Combination Sum/code.py
+++ backtracking/39. Combination Sum/code.py
@@ -2,10 +2,49 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        pass        
+        n = len(candidates)
+
+        # Store all combinations
+        result = []
+
+        # Current combination
+        path = []
+
+        # Calculating sum
+        total = 0
+
+        def backtrack(i, path, total):
+            # # If we find sum, add the combination
+            if total == target:
+                result.append(path.copy())
+                return
+
+            # Invalid sum
+            if i == n or target < total:
+                return
+            
+            # Pick candidates[i]
+            path.append(candidates[i])
+
+            # Many occurences
+            backtrack(i, path, total+candidates[i])
+
+            # UNDO the step
+            path.pop()
+
+            # Call for next element
+            backtrack(i+1, path, total)
+
+        # Call bactrack function for 1st element
+        backtrack(0, path, total)
+
+        return result
 
 
 obj = Solution()
 print(obj.combinationSum([2, 3, 6, 7], 7))              # [[2,2,3],[7]]
 print(obj.combinationSum([2, 3, 5], 8))                 # [[2,2,2,2],[2,3,3],[3,5]]
 print(obj.combinationSum([2], 1))                       # []
+
+# T.C: O(K * 2^T), O(N ^ (T/M))
+# S.C: O(N)
---
+++ backtracking/40. Combination Sum II/code.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        pass
+
+
+obj = Solution()
+print(obj.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))       # [[1,1,6], [1,2,5], [1,7], [2,6]]
+print(obj.combinationSum2([2, 5, 2, 1, 2], 5))              # [[1,2,2], [5]]
