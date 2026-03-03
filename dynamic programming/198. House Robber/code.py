86cadc7cbf1f335e942a379eca7e1be0c3cbe68b
waleed1857 <waleed.cool1857@gmail.com>
2026-03-03 12:39:41 +0500

    Add leetcode-746

---
+++ dynamic programming/198. House Robber/code.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.rob([1, 2, 3, 1]))            # 4
+print(obj.rob([2, 7, 9, 3, 1]))         # 12
+
+# T.C: O(N)
+# S.C: O(N)
--- dynamic programming/746. Min Cost Climbing Stairs/code.py
+++ dynamic programming/746. Min Cost Climbing Stairs/code.py
@@ -2,9 +2,22 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        pass
+        n = len(cost)
+        
+        # Initialize dp table
+        dp = [0] * (n + 1)
+
+        # Minimum of previous 2 "total costs"
+        for i in range(2, n+1):
+            dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+
+        # Return answer for nth number (given)
+        return dp[n]
 
 
 obj = Solution()
 print(obj.minCostClimbingStairs([10, 15, 20]))                                  # 15
 print(obj.minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))          # 6
+
+# T.C: O(N)
+# S.C: O(N)
