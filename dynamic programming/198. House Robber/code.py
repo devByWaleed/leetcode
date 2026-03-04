563b4fca2e5b87eb635f7c2b92b4c31e5412d225
waleed1857 <waleed.cool1857@gmail.com>
2026-03-04 13:11:00 +0500

    Add leetcode-198

--- dynamic programming/198. House Robber/code.py
+++ dynamic programming/198. House Robber/code.py
@@ -2,7 +2,25 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+
+        # Edge cases
+        if n == 1: return nums[0]
+        if n == 2: return max(nums[0], nums[1])
+        
+        # Initialize dp table
+        dp = [0] * (n)
+
+        # Initial cases
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        # Maximum b/w left or robbed
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+
+        # Return answer for n-1
+        return dp[n-1]
 
 
 obj = Solution()
---
+++ dynamic programming/213. House Robber II/code.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.rob([2, 3, 2]))            # 3
+print(obj.rob([1, 2, 3, 1]))         # 4
+print(obj.rob([1, 2, 3]))            # 3
+
+# T.C: O(N)
+# S.C: O(N)
