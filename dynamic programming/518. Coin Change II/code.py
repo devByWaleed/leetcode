68e3d55b922677aadebb05ede841a9ee1db6056a
waleed1857 <waleed.cool1857@gmail.com>
2026-04-16 11:35:24 +0500

    Add leetcode-518

--- dynamic programming/518. Coin Change II/code.py
+++ dynamic programming/518. Coin Change II/code.py
@@ -2,10 +2,44 @@ from typing import List
 
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        pass
+        n = len(coins)
 
+        # Memo table
+        memo = {}
+          
+        def solve(i, a):
+            # Lookup in memo for quick result
+            if (i, a) in memo:
+                return memo[(i, a)]
+            
+            # Base case: we get amount from that coin
+            if a == amount:
+                return 1
+            
+            # Base case: we get bigger amount from that coin
+            if a > amount:
+                return 0
+            
+            # Base case: we check all coins
+            if i >= n:
+                return 0
+            
+            # TAKE: current coin (stay at same index)
+            take = solve(i, a + coins[i])
+
+            # SKIP: move coin to next step
+            skip = solve(i + 1, a)
+            
+            memo[(i, a)] = take + skip
+            return memo[(i, a)]
+        
+        return solve(0, 0)
+       
 
 obj = Solution()
 print(obj.change(5, [1, 2, 5]))             # 4
 print(obj.change(3, [2]))                   # 0
 print(obj.change(10, [10]))                 # 1
+
+# T.C: O(N)
+# S.C: O(N)
---
+++ dynamic programming/62. Unique Paths/code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.uniquePaths(3, 7))             # 28
+print(obj.uniquePaths(3, 2))             # 3
