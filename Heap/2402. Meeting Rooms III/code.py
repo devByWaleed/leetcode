a1aeef18fad62203c67a490bde32ce7e9eadcd5e
waleed1857 <waleed.cool1857@gmail.com>
2026-05-01 10:19:04 +0500

    Add leetcode-253

---
+++ Heap/2402. Meeting Rooms III/code.py
@@ -0,0 +1,11 @@
+from typing import List
+import heapq
+
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.mostBooked(2, [[0,10], [1,5], [2,7], [3,4]]))             # 0
+print(obj.mostBooked(3, [[1,20], [2,10], [3,5], [4,9], [6,8]]))     # 1
--- Heap/253. Meeting Rooms II/code.py
+++ Heap/253. Meeting Rooms II/code.py
@@ -1,10 +1,42 @@
 from typing import List
+import heapq
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        pass
+        # Edge case
+        if len(intervals) == 1:
+            return 1
+        
+        # Sort by starti
+        sorted_intervals = sorted(intervals, key=lambda x: x[0])
+        
+        # Define Heap
+        min_heap = []
+        
+        # Add 1st room manually & heapify
+        min_heap.append(sorted_intervals[0][1])
+        heapq.heapify(min_heap)
+
+        for i in range(1, len(sorted_intervals)):
+            # Get starti, endi
+            starti = sorted_intervals[i][0]
+            endi = sorted_intervals[i][1]
+
+            # If true, means meeting has ended
+            if starti >= min_heap[0]:
+                heapq.heappop(min_heap)
+                heapq.heappush(min_heap, endi)
+            # Means meeting isn't done yet
+            else:
+                heapq.heappush(min_heap, endi)
+
+        # No. of conference rooms
+        return len(min_heap)
 
 
 obj = Solution()
 print(obj.minMeetingRooms([[0,30], [5,10], [15,20]]))        # 2
 print(obj.minMeetingRooms([[7,10], [2,4]]))                  # 1
+
+# T.C: O(N LOG N)
+# S.C: O(N)
