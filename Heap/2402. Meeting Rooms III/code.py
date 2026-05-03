baa1c64ca75cb1a0b17a356797e3d77b0868f1fb
waleed1857 <waleed.cool1857@gmail.com>
2026-05-03 12:03:57 +0500

    Add leetcode-2402

--- Heap/2402. Meeting Rooms III/code.py
+++ Heap/2402. Meeting Rooms III/code.py
@@ -3,9 +3,44 @@ import heapq
 
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        pass
+        # Sort by starti
+        meetings.sort()
+
+        # Free Heap
+        available = [i for i in range(0, n)]
+
+        # Busy Heap
+        used = []
+
+        # Keep track of maximum meetings by every room
+        count = [0] * n
+
+        for start, end in meetings:
+            # Finish meeting
+            while used and start >= used[0][0]:
+                _, room = heapq.heappop(used)
+                heapq.heappush(available, room)
+
+            # If no room available
+            if not available:
+                endi, room = heapq.heappop(used)
+                end = endi + (end - start)
+                heapq.heappush(available, room)
+
+            # Using available room
+            room = heapq.heappop(available)
+            heapq.heappush(used, [end, room])
+
+            # Current room get 1 meeting
+            count[room] += 1
+
+        # Return room conducts maximum meetings
+        return count.index(max(count))
 
 
 obj = Solution()
 print(obj.mostBooked(2, [[0,10], [1,5], [2,7], [3,4]]))             # 0
 print(obj.mostBooked(3, [[1,20], [2,10], [3,5], [4,9], [6,8]]))     # 1
+
+# T.C: O(M LOG M + M LOG N)
+# S.C: O(M LOG M + M LOG N)
---
+++ Heap/502. IPO/code.py
@@ -0,0 +1,11 @@
+from typing import List
+import heapq
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.findMaximizedCapital(2, 0, [1,2,3], [0,1,1]))     # 4
+print(obj.findMaximizedCapital(3, 0, [1,2,3], [0,1,2]))     # 6
