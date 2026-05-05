c8a4a95f5d5ff305eb42adc89c579495568d09f5
waleed1857 <waleed.cool1857@gmail.com>
2026-05-05 11:41:19 +0500

    Add leetcode-502

---
+++ Heap/295. Find Median from Data Stream/code.py
@@ -0,0 +1,31 @@
+class MedianFinder:
+
+    def __init__(self):
+        pass
+        
+
+    def addNum(self, num: int) -> None:
+        pass
+        
+
+    def findMedian(self) -> float:
+        pass
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+obj = MedianFinder()
+obj.addNum(1)
+obj.addNum(2)
+print(obj.findMedian())     # 1.5
+obj.addNum(3)
+print(obj.findMedian())     # 2.0
+# Output:
+'''
+None
+None
+None
+1.5
+None
+2.0
+'''
--- Heap/502. IPO/code.py
+++ Heap/502. IPO/code.py
@@ -3,9 +3,43 @@ import heapq
 
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        pass
+        # Using to remain the input same
+        c = w
+        n = len(profits)
+
+        # MAX_HEAP stores max profit
+        max_heap = []
+        
+        # Pairing capital and profit
+        projects = []
+        for i in range(n):
+            projects.append((capital[i], profits[i]))
+        
+        # Sort by capital
+        projects.sort()
+
+        # Counter to track current project
+        counter = 0
+
+        for _ in range(k):
+            # Adding profit to heap
+            while counter < n and projects[counter][0] <= c:
+                heapq.heappush(max_heap, -projects[counter][1])
+                counter += 1
+            
+            # If empty we can't start new project
+            if not max_heap:
+                break
+
+            # Pop the profit and add it to final capital
+            c += -heapq.heappop(max_heap)
+
+        return c
 
 
 obj = Solution()
 print(obj.findMaximizedCapital(2, 0, [1,2,3], [0,1,1]))     # 4
 print(obj.findMaximizedCapital(3, 0, [1,2,3], [0,1,2]))     # 6
+
+# T.C: O(N LOG N + K LOG N)
+# S.C: O(N LOG N + K LOG N) -> O(N)
