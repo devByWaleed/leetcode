66a1ab97efd0752c022ac2fdb3c0e0f96e795134
waleed1857 <waleed.cool1857@gmail.com>
2026-05-18 20:57:41 +0500

    Template

---
+++ greedy/53. Maximum Subarray/code.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))     # 6
+print(obj.maxSubArray([1]))     # 1
+print(obj.maxSubArray([5, 4, -1, 7, 8]))     # 23
