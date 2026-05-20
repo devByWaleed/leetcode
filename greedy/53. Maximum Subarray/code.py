c383f0524e44feeb8e393aa8b174900fbeb3da0f
waleed1857 <waleed.cool1857@gmail.com>
2026-05-20 11:30:57 +0500

    Add leetcode-53

--- greedy/53. Maximum Subarray/code.py
+++ greedy/53. Maximum Subarray/code.py
@@ -2,10 +2,32 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        # Edge case
+        if n == 1:
+            return nums[0]
+        
+        curr_sum = 0
+        max_sum = -float("inf")
+
+        for i in range(n):
+            # Adding current number
+            curr_sum += nums[i]
+
+            # Updating max
+            max_sum = max(max_sum, curr_sum)
+
+            # If current is negative, reset to 0
+            if curr_sum < 0:
+                curr_sum = 0
+
+        return max_sum
 
 
 obj = Solution()
 print(obj.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))     # 6
 print(obj.maxSubArray([1]))     # 1
 print(obj.maxSubArray([5, 4, -1, 7, 8]))     # 23
+
+# T.C: O(N)
+# S.C: O(1)
---
+++ greedy/763. Partition Labels/code.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        pass
+
+
+obj = Solution()
+print(obj.partitionLabels("ababcbacadefegdehijhklij"))      # [9, 7, 8]
+print(obj.partitionLabels("eccbbbbdec"))                    # [10]
