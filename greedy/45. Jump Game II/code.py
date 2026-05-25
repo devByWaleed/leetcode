9806997304710d6347f392ee14cc90e0c45806c3
waleed1857 <waleed.cool1857@gmail.com>
2026-05-25 18:59:16 +0500

    Add leetcode-678

---
+++ greedy/45. Jump Game II/code.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.jump([2, 3, 1, 1, 4]))      # 2
+print(obj.jump([2, 3, 0, 1, 4]))      # 2
--- greedy/678. Valid Parenthesis String/code.py
+++ greedy/678. Valid Parenthesis String/code.py
@@ -1,9 +1,46 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        pass
+        # 2 var greedy approach 
+
+        # min -> ) & max for (
+        min_open, max_open = 0, 0
+
+        for char in s:
+
+            if char == "(":
+                min_open += 1
+                max_open += 1
+            
+            if char == "*":
+                # * -> )
+                min_open -= 1
+                # * -> (
+                max_open += 1
+
+            if char == ")":
+                # * -> )
+                min_open -= 1
+                # * -> (
+                max_open -= 1
+            
+            # If -ve, we assume to use * as ""
+            if min_open < 0:
+                min_open = 0
+            
+            # If -ve, we could not balance the parentheses
+            if max_open < 0:
+                return False
+
+        # There is a way to make string balanced
+        return min_open == 0
 
 
 obj = Solution()
 print(obj.checkValidString("()"))      # True
 print(obj.checkValidString("(*)"))     # True
 print(obj.checkValidString("(*))"))    # True
+print(obj.checkValidString("("))       # False
+print(obj.checkValidString("(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((("))      # False
+
+# T.C: O(N)
+# S.C: O(1)
