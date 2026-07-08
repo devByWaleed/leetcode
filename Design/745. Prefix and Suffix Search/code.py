4c9de3f5efac7e1c06cc7c6bd4caab17d37ed528
waleed1857 <waleed.cool1857@gmail.com>
2026-07-08 10:08:09 +0500

    Add leetcode-648

---
+++ Design/745. Prefix and Suffix Search/code.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class WordFilter:
+
+    def __init__(self, words: List[str]):
+        pass
+        
+
+    def f(self, pref: str, suff: str) -> int:
+        pass
+        
+
+
+# Your WordFilter object will be instantiated and called as such:
+obj = WordFilter("apple")
+print(obj.f("a", "e"))
--- Trie/648. Replace Words/code.py
+++ Trie/648. Replace Words/code.py
@@ -1,13 +1,66 @@
 from typing import List
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}      # Maps character -> TrieNode
+        self.is_terminal = False  # Marks the end of a valid word
+
+
 class Solution:
+    def __init__(self):
+        # Initialize the tree with a root node
+        self.root = TrieNode()
+
+    def createTrie(self, root_word):
+        # Pointer
+        curr = self.root
+
+        for char in root_word:
+            # If the character path doesn't exist, create it
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            
+            # Moving down selected character branch
+            curr = curr.children[char]
+        
+        # Mark the end of the word
+        curr.is_terminal = True
+
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        pass
+        # Building Trie
+        for root_word in dictionary:
+            self.createTrie(root_word)
+        
+        # Splitting into array
+        sen_arr = sentence.split(" ")
+
+
+        for i in range(len(sen_arr)):
+            # Pointer in outer loop for correct position tracking
+            curr = self.root
+            # Single word which will place in
+            prefix = ""
+            for char in sen_arr[i]:
+                # If a character is missing, the exact word doesn't exist
+                if char not in curr.children:
+                    break
+                else:
+                    curr = curr.children[char]
+                    prefix += char
+
+                    # If Trie word ends
+                    if curr.is_terminal:
+                        sen_arr[i] = prefix
+                        break
+                
+        # Making final string
+        ans = " ".join(sen_arr)
+        return ans
 
 
 obj = Solution()
 print(obj.replaceWords(["cat","bat","rat"], "the cattle was rattled by the battery"))      # "the cat was rat by the bat"
 print(obj.replaceWords(["a","b","c"], "aadsfasf absbs bbab cadsfafs"))      # "a a b c"
 
-# T.C: 
-# S.C: 
+# T.C: O(d×l + s×l)
+# S.C: O(d×l + s)
