43238c4299a3dbaaaf4dbaeb3bb2586644bdfaea
waleed1857 <waleed.cool1857@gmail.com>
2026-07-15 11:47:12 +0500

    Add leetcode-190

---
+++ Bit-Manipulation/136. Single Number/code.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.singleNumber([2,2,1]))      # 1
+print(obj.singleNumber([4,1,2,1,2]))      # 4
+print(obj.singleNumber([1]))      # 1
+
+# T.C: O()
+# S.C: O()
--- Bit-Manipulation/190. Reverse Bits/code.py
+++ Bit-Manipulation/190. Reverse Bits/code.py
@@ -1,8 +1,21 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        pass
+        reverse_bit = 0
+        num = n
+
+        for _ in range(32):
+            # Merged = (Shifting bit for allocating space) + LSB
+            reverse_bit = (reverse_bit << 1) | (num & 1)
+
+            # Discard LSB we get earlier
+            num = num >> 1
+
+        return reverse_bit
 
 
 obj = Solution()
 print(obj.reverseBits(43261596))      # 964176192
 print(obj.reverseBits(2147483644))      # 1073741822
+
+# T.C: O(1)
+# S.C: O(1)
