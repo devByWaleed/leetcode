a41e72c2b8cdae24ef812760a92d2729ccc24442
waleed1857 <waleed.cool1857@gmail.com>
2026-07-16 11:32:41 +0500

    Add leetcode-136

--- Bit-Manipulation/136. Single Number/code.py
+++ Bit-Manipulation/136. Single Number/code.py
@@ -2,7 +2,14 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        pass
+        # Track that single number
+        track = 0
+
+        for i in range(len(nums)):
+            # It will overwrite the track if same number occurs again.
+            track = nums[i] ^ track
+        
+        return track
 
 
 obj = Solution()
@@ -10,5 +17,5 @@ print(obj.singleNumber([2,2,1]))      # 1
 print(obj.singleNumber([4,1,2,1,2]))      # 4
 print(obj.singleNumber([1]))      # 1
 
-# T.C: O()
-# S.C: O()
+# T.C: O(N)
+# S.C: O(1)
