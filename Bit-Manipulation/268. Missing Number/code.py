065ac5b25920b0459260c43ead7eea9fd3b6020f
waleed1857 <waleed.cool1857@gmail.com>
2026-07-17 09:19:53 +0500

    Add leetcode-268

--- Bit-Manipulation/268. Missing Number/code.py
+++ Bit-Manipulation/268. Missing Number/code.py
@@ -1,3 +1,39 @@
+# XOR operator
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        xor_array = 0
+        xor_range = 0
+
+        # XORing array elements
+        for number in nums:
+            xor_array = xor_array ^ number
+
+        # XORing numbers in given range
+        for i in range(0, n+1):
+            xor_range = xor_range ^ i
+
+        # Xoring the number itself cancels out, res will be missing number
+        res = xor_array ^ xor_range
+        return res
+    
+
+obj = Solution()
+
+print(obj.missingNumber([3, 0, 1]))                     # 2
+print(obj.missingNumber([0, 1]))                        # 2
+print(obj.missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))   # 8
+
+# T.C = O(N)
+# S.C = O(1)
+
+
+
+
+# Using Math
+'''
 from typing import List
 
 class Solution:
@@ -23,3 +59,4 @@ print(obj.missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))   # 8
 
 # T.C = O(N)
 # S.C = O(1)
+'''
---
+++ Bit-Manipulation/371. Sum of Two Integers/code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.getSum(1, 2))     # 3
+print(obj.getSum(2, 3))     # 5
