ec759c38dbc2179729b0a415f63269a89023a8fb
waleed1857 <waleed.cool1857@gmail.com>
2026-07-18 21:01:59 +0500

    Add leetcode-371

--- Bit-Manipulation/371. Sum of Two Integers/code.py
+++ Bit-Manipulation/371. Sum of Two Integers/code.py
@@ -1,8 +1,35 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        pass
+        # Mask of 32-bit to handle Python's integer
+        mask = 0xFFFFFFFF
+        
+        # Save copy
+        num1, num2 = a & mask, b & mask
+        
+        #while num2 != 0:
+        while num1 != 0:
+            # Save carry for bigger numbers
+            carry = (num1 & num2) & mask
+            
+            # XOR done sum without carry
+            #num1 = (num1 ^ num2) & mask
+            num2 = (num1 ^ num2) & mask
+            
+            # Shift carry by 1 and store it for later sum
+            #num2 = (carry << 1) & mask
+            num1 = (carry << 1) & mask
+         
+        # If number is -ve, convert num2 back to -ve
+        if num2 > 0x7FFFFFFF:
+            return ~(num2 ^ mask)
+            
+        return num2
+        #return num1
 
 
 obj = Solution()
 print(obj.getSum(1, 2))     # 3
 print(obj.getSum(2, 3))     # 5
+
+# T.C: O(1)
+# S.C: O(1)
---
+++ Bit-Manipulation/7. Reverse Integer/code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        pass
+
+
+obj = Solution()
+print(obj.reverse(123))     # 321
+print(obj.reverse(-123))     # -321
+print(obj.reverse(120))     # 21
