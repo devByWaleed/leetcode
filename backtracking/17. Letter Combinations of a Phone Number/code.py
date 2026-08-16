58a8109e39d8bdbe63610b26c1d7c0ef16fa99b4
waleed1857 <waleed.cool1857@gmail.com>
2026-08-16 19:50:44 +0500

    Solve leetcode-17

--- backtracking/17. Letter Combinations of a Phone Number/code.py
+++ backtracking/17. Letter Combinations of a Phone Number/code.py
@@ -3,15 +3,6 @@ from typing import List
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         n = len(digits)
-        
-        # Base case
-        if n == 0:  return []
-
-        # Store all combinations
-        result = []
-
-        # Currnt combinations
-        pair = []
 
         # Mapping
         letter_map = {
@@ -20,32 +11,36 @@ class Solution:
             "7": "pqrs", "8": "tuv", "9": "wxyz"
         }
 
-        def backtrack(i):
-            # If we get combination
-            if i == n:
-                result.append("".join(pair))
-                return
-
-            for letter in letter_map[digits[i]]:
-                # Add letter
-                pair.append(letter)
-
-                # Call for next element
-                backtrack(i+1)
-
-                # UNDO the step
-                pair.pop()
-
+        # Stores final answer
+        result = []
         
-        # Call bactrack function for 1st element
+        # Stores single pairs
+        pairs = []
+        
+        def backtrack(i):
+            # Base case to add into result
+            if i == n:
+                result.append("".join(pairs))
+                return
+            
+            for letter in letter_map[digits[i]]:
+                # Add to pairs
+                pairs.append(letter)
+                
+                # Backtrack
+                backtrack(i+1)
+                
+                # POP ( UNDO )
+                pairs.pop()
+        
+        # Call function with initial value
         backtrack(0)
         return result
 
 
 obj = Solution()
-
 print(obj.letterCombinations("23"))     # ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 print(obj.letterCombinations("2"))      # ["a","b","c"]
 
-# T.C: O(4^N)
-# S.C: O(N)
+# T.C: O(4^N) --> Max mapping + Looking for each combination + creating copy
+# S.C: O(N) --> Recursive call stack used
