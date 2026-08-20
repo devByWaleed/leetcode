09af0c205a07664b2f15413b16e8ceb63d8a4967
waleed1857 <waleed.cool1857@gmail.com>
2026-08-20 12:10:55 +0500

    Solve leetcode-131

--- backtracking/131. Palindrome Partitioning/code.py
+++ backtracking/131. Palindrome Partitioning/code.py
@@ -3,36 +3,36 @@ from typing import List
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         n = len(s)
-
-        # Store all palindrome
+                        
+        # Stores final answer
         result = []
-
-        # Currnt palindrome
+        
+        # Stores single palindrome
         pal = []
 
         def backtrack(i):
-            # If we get all palindromes
+            # Base case to add into result
             if i >= n:
                 result.append(pal.copy())
                 return
-            
-            # Checking for all possible palindrome
+
             for j in range(i, n):
+                # Checking for palindrome
                 if self.is_pal(s, i, j):
-                    # Add palindrome
+                    # Add string with help of indexing
                     pal.append(s[i: j+1])
 
-                    # Call for next element
+                    # Backtrack: New element
                     backtrack(j+1)
 
-                    # UNDO the step
+                    # POP ( UNDO )
                     pal.pop()
 
 
-        # Call bactrack function for 1st element
+        # Call function with initial value
         backtrack(0)
         return result
-    
+
 
     # Private function to check palindrome
     def is_pal(self, s, l, r):
@@ -44,9 +44,31 @@ class Solution:
 
 
 obj = Solution()
-
 print(obj.partition("aab"))     # [["a","a","b"], ["aa","b"]]
 print(obj.partition("a"))       # [["a"]]
 
-# T.C: O(N * 2^N)
-# S.C: O(N)
+# T.C: O(N * 2^N)  --> N-1 potential cut for N length string
+# S.C: O(N)        --> Recursive call stack used
+
+
+
+# Template
+
+'''
+result, pal = [], []
+
+backtrack(i):
+    Base condition
+
+    Loop till (i,n)
+        # Not picked
+        Add to pal
+
+        backtrack(j+1)
+
+        # UNDO
+        POP from pal
+
+backtrack(0)
+return result
+'''
